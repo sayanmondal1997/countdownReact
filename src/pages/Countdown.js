d45d1797950af764d49fd6f46600f3da3eb17923
aponiar <sayan.mondal@gmail.com>
2020-11-17 17:08:29 +0530

    animation pull

--- src/pages/Countdown.js
+++ src/pages/Countdown.js
@@ -1,58 +1,166 @@
+// import React, { useState, useEffect } from 'react';
+// import { connect } from 'react-redux';
+
+// function CountdownPage(props) {
+//   const sysDate = new Date();
+//   const givenDate = new Date('12/12/2020');
+//   const diff = (givenDate.getTime()- sysDate.getTime()) 
+//   const dateDiff =Math.floor(diff/(1000 * 3600 * 24))
+//   const timeDiff = dateDiff%10
+  
+//   const {initialDays= dateDiff, initialHours=0, initialMinute = 1,initialSeconds = 2} = props;
+//   const [ minutes, setMinutes ] = useState(initialMinute);
+//   const [seconds, setSeconds ] =  useState(initialSeconds);
+//   const [hours, setHours ] =  useState(initialHours);
+//   const [days, setDays ] =  useState(initialDays); 
+ 
+//   useEffect(()=>{
+//     console.log('date',(givenDate.getTime()- sysDate.getTime()))
+//     console.log('diff',timeDiff);
+    
+//   let myInterval = setInterval(() => {
+//           if (seconds > 0) {
+//               setSeconds(seconds - 1);
+//           }
+//           if (seconds === 0) {
+//               if (minutes === 0) {
+//                 if(hours === 0){
+//                   if(days === 0){
+//                     clearInterval(myInterval)
+//                   }
+//                   else{
+//                     setDays(days-1);
+//                     setHours(23);
+//                   }
+//                 }
+//                 else{
+//                   setHours(hours-1);
+//                   setMinutes(59);
+//                 }
+                  
+//               } else {
+//                   setMinutes(minutes - 1);
+//                   setSeconds(59);
+//               }
+//           } 
+//       }, 1000)
+//       return ()=> {
+//           clearInterval(myInterval);
+//         };
+//   });
+
+//   return (
+//       <div>
+//       <h1> {days< 10 ?  `0${days}` : days}:{hours< 10 ?  `0${hours}` : hours}:{minutes< 10 ?  `0${minutes}` : minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
+      
+//       </div>
+//   )
+// }
+// const mapStateToProps = state => state;
+// export default connect(mapStateToProps)(CountdownPage);
+
+
+
+ 
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import Countdown from 'react-countdown';
+import './../App.css';
 
-function CountdownPage(props) {
+function Countdown(props) {
+
   const sysDate = new Date();
-  const givenDate = new Date('12/12/2020');
-  const diff =Math.floor((givenDate.getTime()- sysDate.getTime())/(1000 * 3600 * 24))
-  const {initialDays= diff, initialHours=0, initialMinute = 1,initialSeconds = 2} = props;
-  const [ minutes, setMinutes ] = useState(initialMinute);
-  const [seconds, setSeconds ] =  useState(initialSeconds);
-  const [hours, setHours ] =  useState(initialHours);
-  const [days, setDays ] =  useState(initialDays); 
- 
-  useEffect(()=>{
-    console.log('date',(givenDate.getTime()- sysDate.getTime())/(1000 * 3600 * 24))
-  let myInterval = setInterval(() => {
-          if (seconds > 0) {
-              setSeconds(seconds - 1);
-          }
-          if (seconds === 0) {
-              if (minutes === 0) {
-                if(hours === 0){
-                  if(days === 0){
-                    clearInterval(myInterval)
-                  }
-                  else{
-                    setDays(days-1);
-                    setHours(23);
-                  }
+  const givenDate = new Date(props.countDownStyle.eventDate);
+  const diff = Math.floor(givenDate.getTime() - sysDate.getTime()) 
+  const sec = Math.floor((diff/1000)%60)
+  const min = Math.floor((diff/(1000*60))%60)
+  const hr = Math.floor((diff/(1000*60*60))%24)
+  const day = Math.floor((diff/(1000*60*60*24))%30)
+  const month = Math.floor((diff/(1000*60*60*24*30))%12)
+  const year = Math.floor(diff/(1000*60*60*24*30*12))
+  const {initialYear = year, initialMonth = month, initialDays =  day, initialHours = hr, initialMinute = min, initialSeconds = sec} = props;
+  const [minutes, setMinutes] = useState(initialMinute);
+  const [seconds, setSeconds] = useState(initialSeconds);
+  const [hours, setHours] = useState(initialHours);
+  const [days, setDays] = useState(initialDays);
+  const [months, setMonths] = useState(initialMonth);
+  const [years, setYears] = useState(initialYear);
+
+  useEffect(() => {
+    console.log('date', (givenDate.getTime() - sysDate.getTime()) / (1000 * 3600 * 24))
+    let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          if (hours === 0) {
+            if (days === 0) {
+              if(months === 0){
+                if(years === 0){
+                  clearInterval(myInterval)
+                }else{
+                  setYears(years-1);
+                  setMonths(11);
                 }
-                else{
-                  setHours(hours-1);
-                  setMinutes(59);
-                }
-                  
-              } else {
-                  setMinutes(minutes - 1);
-                  setSeconds(59);
+              }else{
+                setMonths(months-1);
+                setDays(29);
               }
-          } 
-      }, 1000)
-      return ()=> {
-          clearInterval(myInterval);
-        };
+            }else {
+              setDays(days - 1);
+              setHours(23);
+            }
+          }
+          else {
+            setHours(hours - 1);
+            setMinutes(59);
+          }
+
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000)
+    return () => {
+      clearInterval(myInterval);
+    };
   });
 
   return (
-      <div>
-        <Countdown date={Date.now() + 50000}></Countdown>
-      { <h1> {days< 10 ?  `0${days}` : days}:{hours< 10 ?  `0${hours}` : hours}:{minutes< 10 ?  `0${minutes}` : minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
+    <div>
+      {console.log('propssssssssssss', props)
       }
-      </div>
+      {props.countDownStyle ? 
+        <div style={{ paddingTop: '20px', paddingBottom: '20px', background: props.countDownStyle.background ? props.countDownStyle.background : 'cornflowerblue', borderRadius: props.countDownStyle.borderradius ? props.countDownStyle.borderradius : '15px' }}>
+          <h1 style={{ color: props.countDownStyle.textStyle.headercolor ? props.countDownStyle.textStyle.headercolor : 'darkblue', fontSize: props.countDownStyle.textStyle.headerSize ? props.countDownStyle.textStyle.headerSize : '20px', fontFamily: props.countDownStyle.textStyle.fontFamily ? props.countDownStyle.textStyle.fontFamily : 'serif' }}>{props.countDownStyle.eventName} Countdown</h1>
+          {(diff>0) ?
+            <div> 
+              {props.countDownStyle.count ? 
+            <div className="countdown-wrapper">
+
+              {props.countDownStyle.count.y ? <div className="countdown-item"> {years<10 ? `0${years}` : years} <span>Years</span> </div> : null}
+              {props.countDownStyle.count.m ? <div className="countdown-item">{ months<10 ? `0${months}` : months}<span>Months</span></div> : null}
+              {props.countDownStyle.count.d ? <div className="countdown-item">{days < 10 ? `0${days}` : days}<span>Days</span></div> : null}
+              {props.countDownStyle.count.h ? <div className="countdown-item">{hours < 10 ? `0${hours}` : hours}<span>Hours</span></div> : null}
+              {props.countDownStyle.count.min ? <div className="countdown-item">{minutes < 10 ? `0${minutes}` : minutes}<span>minutes</span></div> : null}
+              {props.countDownStyle.count.s ? <div className="countdown-item">{seconds < 10 ? `0${seconds}` : seconds}<span>Seconds</span></div> : null}
+              </div>
+              :
+              <div>
+              { years<10 ? `0${years}` : years}:{ months<10 ? `0${months}` : months}:{days < 10 ? `0${days}` : days}:{hours < 10 ? `0${hours}` : hours}:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+              </div>}
+            </div>
+            :
+            <span style={{ color: props.countDownStyle.textStyle.timeleftColor ? props.countDownStyle.textStyle.timeleftColor : 'darkred', fontSize: props.countDownStyle.textStyle.timeleftSize ? props.countDownStyle.textStyle.timeleftSize : '30px', fontFamily: props.countDownStyle.textStyle.fontFamily ? props.countDownStyle.textStyle.fontFamily : 'serif' }}>Time's up!</span>
+          }
+        </div> 
+        : 
+        <></>
+      }
+
+    </div>
   )
 }
 const mapStateToProps = state => state;
-export default connect(mapStateToProps)(CountdownPage);
-
+export default connect(mapStateToProps)(Countdown);
--- src/pages/initial.js
+++ src/pages/initial.js
@@ -1,13 +1,63 @@
+// import React, { useState, useEffect } from 'react';
+// import { connect } from 'react-redux';
+// import { GetConfigurationAction } from './../actions/GetConfigurationAction';
+// import { useHistory } from "react-router-dom";
+
+// function Initial(props) {
+//   const [count, setCount] = useState(6);
+//   const [loading, loadingfunc] = useState(true);
+//   // const count=0;
+//   const history = useHistory();
+// //   const handleClick = () => history.push('/goodbye');
+//   useEffect(() => {
+//     props.dispatch(GetConfigurationAction((res) => {
+//       console.log('config response', res)
+//     }))
+//   }, []);
+
+//   var click = (props) => {
+//       console.log('jbjj',props);
+      
+//     // props.dispatch(GetConfigurationAction((res) => {
+//     //   console.log('config response', res)
+//       loadingfunc(false)
+//       history.push(`/countdown`)
+//     //   console.log('sayanljj')
+//     // }))
+
+
+//   }
+
+//   return (
+//     <div className="App">
+//       <div>
+//         {loading &&
+//           <p>You clicked {count} times</p>}
+//         <button onClick={() => [setCount(count + 1), loadingfunc(true)]}>
+//           Count
+//       </button>
+//         <button onClick={() => click(props)}>
+//           Click me
+//       </button>
+//       </div>
+//     </div>
+//   );
+// }
+// const mapStateToProps = state => state;
+// export default connect(mapStateToProps)(Initial);
+
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { GetConfigurationAction } from './../actions/GetConfigurationAction';
-import { useHistory } from "react-router-dom";
+import Countdown from './Countdown';
+// import { useHistory } from "react-router-dom";
 
 function Initial(props) {
+  const [clicked, handleClick] = useState(false);
   const [count, setCount] = useState(6);
   const [loading, loadingfunc] = useState(true);
   // const count=0;
-  const history = useHistory();
+//   const history = useHistory();
 //   const handleClick = () => history.push('/goodbye');
   useEffect(() => {
     props.dispatch(GetConfigurationAction((res) => {
@@ -17,11 +67,12 @@ function Initial(props) {
 
   var click = (props) => {
       console.log('jbjj',props);
+      handleClick(true);
       
     // props.dispatch(GetConfigurationAction((res) => {
     //   console.log('config response', res)
       loadingfunc(false)
-      history.push(`/countdown`)
+      // props.history.push(`/countdown`)
     //   console.log('sayanljj')
     // }))
 
@@ -40,9 +91,9 @@ function Initial(props) {
           Click me
       </button>
       </div>
+      {clicked ? <Countdown  countDownStyle={{count: {y: true, m: true, d: true, h: true, min: true, s: true}, eventName: 'Event', eventDate: '05/07/2030', background: 'yellow', textStyle: {headercolor: 'black', timeleftColor: 'black', headerSize: '20px', timeleftSize: '25px', fontFamily: 'fantasy'}, borderradius: '10px'}} ></Countdown> : null}
     </div>
   );
 }
 const mapStateToProps = state => state;
 export default connect(mapStateToProps)(Initial);
-
